17122a5d218e121a5b9ee2ae128c3939d1648e54
ehtesam75 <ehtesamzunnuryn@gmail.com>
2024-12-27 14:31:52 +0600

    add B. Bad Boy

---
+++ Basics/B. Bad Boy.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FASTER ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define endl '\n'
+
+void solve(){
+    int n, m, i , j; cin >> n >> m >> i >> j;
+    cout << 1 << ' ' << 1 << ' ' << n << ' ' << m << endl;
+}
+
+int32_t main(){
+    FASTER;
+    int tc; cin >> tc;
+    
+    while(tc--){
+        solve();
+    }
+
+    return 0;
+}
+
+//https://codeforces.com/problemset/problem/1537/B
